bf299afedd891641cbc4ab3126d8a9cb82fa7d45
DipanPramanik098 <myselfdipan6@gmail.com>
2026-06-14 15:07:13 +0530

    TypeCasting In Java

---
+++ Session_004/_01_Type_Conversion.java
@@ -0,0 +1,19 @@
+public class _01_Type_Conversion {
+    public static void main(String[] args) {
+        byte b = 8;
+        int i = b;
+        System.out.println(i);
+
+        int ii=300;
+        byte bb = (byte) ii;
+        System.out.println(bb);
+
+        char c = 'a';
+        int iii = c;
+        System.out.println(iii);
+
+        float f = 1.009f;
+        int iiii = (int) f;
+        System.out.println(iiii);
+    }
+}
